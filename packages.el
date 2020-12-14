491c9c4ab40a763179d80ff35fdd39739e179392
Tom Purl <tom.purl@wi-tronix.com>
2020-12-14 08:52:25 -0600

    Add go and ox-gfm integration

--- packages.el
+++ packages.el
@@ -27,6 +27,7 @@
 (package! bongo)   ;; media player
 (package! ag)
 (package! kaolin-themes)
+(package! ox-gfm)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
