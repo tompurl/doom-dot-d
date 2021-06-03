2aea0da50e6921b7b90ed4893b4ba366756cf290
Tom Purl <tom.purl@wi-tronix.com>
2021-06-03 14:07:00 -0500

    Add module for archiving web sites as org files

--- packages.el
+++ packages.el
@@ -40,6 +40,7 @@
 (package! modus-themes)
 (package! writefreely)
 (package! wwg)
+(package! org-web-tools)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
