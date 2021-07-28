3ea2510e6fda115ccb58cff898e0c1336d655a7a
Tom Purl <tom.purl@wi-tronix.com>
2021-07-28 08:59:26 -0500

    Little stuff
    Powershell support, eink-theme addition, and a eshell snippet

--- packages.el
+++ packages.el
@@ -46,6 +46,8 @@
 ;; https://github.com/raxod502/straight.el#the-recipe-format
 ;(package! another-package
 ;  :recipe (:host github :repo "username/repo"))
+(package! eink-theme
+ :recipe (:host github :repo "maio/eink-emacs"))
 
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
