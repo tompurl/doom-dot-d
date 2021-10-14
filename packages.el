9e45f99db8cedc8e98183afac7707d31f4a664cf
Tom Purl <tom.purl@wi-tronix.com>
2021-10-14 14:52:52 -0500

    Add lots of little things, mostly UI-related

--- packages.el
+++ packages.el
@@ -40,6 +40,7 @@
 (package! writefreely)
 (package! wwg)
 (package! org-web-tools)
+(package! centaur-tabs)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
